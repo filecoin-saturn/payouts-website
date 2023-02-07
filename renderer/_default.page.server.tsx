b6adf0d2d21e9f14b61af002ef9985e55e22b96f
ameanasad <amean.asad1999@gmail.com>
2023-02-07 17:06:49 -0500

    feat: setup routing and ssr

---
+++ renderer/_default.page.server.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr';
+
+import { PageShell } from './PageShell';
+import type { PageContextServer } from './types';
+
+export { render };
+// See https://vite-plugin-ssr.com/data-fetching
+export const passToClient = ['pageProps', 'urlPathname'];
+
+async function render(pageContext: PageContextServer) {
+    const { Page, pageProps } = pageContext;
+    const pageHtml = ReactDOMServer.renderToString(
+        <PageShell pageContext={pageContext}>
+            <Page {...pageProps} />
+        </PageShell>
+    );
+
+    // See https://vite-plugin-ssr.com/head
+    const { documentProps } = pageContext.exports;
+    const title = (documentProps && documentProps.title) || 'Vite SSR app';
+    const desc =
+        (documentProps && documentProps.description) ||
+        'App using Vite + vite-plugin-ssr';
+
+    const documentHtml = escapeInject`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="${desc}" />
+        <title>${title}</title>
+      </head>
+      <body>
+        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
+      </body>
+    </html>`;
+
+    return {
+        documentHtml,
+        pageContext: {
+            // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
+        },
+    };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import ssr from 'vite-plugin-ssr/plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), ssr()],
 });
