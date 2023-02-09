1a1a18d1015bbaabcbd884946d6c7bdb7702828f
ameanasad <amean.asad1999@gmail.com>
2023-02-09 14:17:56 -0500

    feat: add test suite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import ssr from 'vite-plugin-ssr/plugin';
+import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), ssr()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './setup.ts',
+        globals: true,
+    },
 });
