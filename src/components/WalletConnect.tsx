2a1f6e55ebfb6c8b8107b79650ff19a3aa718c04
ameanasad <amean.asad1999@gmail.com>
2023-02-13 15:37:44 -0500

    feat: Connect from multiple wallets

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,53 +1,81 @@
-import { Box, Button, Center, VStack } from '@chakra-ui/react';
-import { WagmiConfig } from 'wagmi';
-import { useAccount, useConnect } from 'wagmi';
+import {
+    Button,
+    Center,
+    Heading,
+    UseToastOptions,
+    VStack,
+} from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { Connector, useConnect } from 'wagmi';
 
-import { client } from '../utils/wallet-utils';
-import Contract from './Contract';
-
+import { switchNetwork } from '../utils/contract-utils';
 function Profile() {
     const { connect, connectors, error, isLoading, pendingConnector } =
         useConnect();
 
-    const { address, isConnecting, isDisconnected } = useAccount();
+    const toast = useToast();
+
+    const userConnect = async (connector: Connector) => {
+        if (window.ethereum) {
+            await switchNetwork();
+        }
+        connect({ connector });
+    };
+    useEffect(() => {
+        if (error) {
+            toast.closeAll();
+            const toastInfo: UseToastOptions = {
+                title: 'Error Connecting',
+                description: '',
+                status: 'error',
+                duration: 5000,
+                position: 'top',
+                isClosable: true,
+            };
+            if (error instanceof Error) {
+                toastInfo.description = error.message;
+            }
+            toast(toastInfo);
+        }
+    });
 
     return (
-        <Box backgroundColor={'blue.900'} h="100vh" w="100%">
-            <Center w="100%" h="100%">
-                <VStack
-                    w="100%"
-                    maxWidth={'800px'}
-                    backgroundColor={'whiteAlpha.900'}
-                    padding={'5'}
-                    spacing="4"
-                >
-                    {connectors.map((connector) => (
-                        <Button
-                            disabled={!connector.ready}
-                            key={connector.id}
-                            w="250px"
-                            size={'lg'}
-                            onClick={() => connect({ connector })}
-                        >
-                            {connector.name}
-                            {!connector.ready}
-                            {isLoading && connector.id === pendingConnector?.id}
-                        </Button>
-                    ))}
-                </VStack>
-            </Center>
+        <Center w="100%" h="80%">
+            <VStack
+                maxWidth={'800px'}
+                backgroundColor={'whiteAlpha.200'}
+                padding={8}
+                rounded="md"
+                spacing="8"
+            >
+                <Heading color={'whiteAlpha.900'}>Sign in with wallet</Heading>
 
-            {/* {address && <Contract address={address} />} */}
-            {/* {error && <div>{error.message}</div>} */}
-        </Box>
+                {connectors.map((connector) => (
+                    <Button
+                        disabled={!connector.ready}
+                        key={connector.id}
+                        isLoading={
+                            isLoading && connector.id === pendingConnector?.id
+                        }
+                        loadingText="Connecting"
+                        spinnerPlacement="end"
+                        w="400px"
+                        h="70px"
+                        size={'lg'}
+                        onClick={() => userConnect(connector)}
+                    >
+                        {connector.name}
+                        {!connector.ready}
+                        {isLoading && connector.id === pendingConnector?.id}
+                    </Button>
+                ))}
+            </VStack>
+        </Center>
     );
 }
 
 // Pass client to React Context Provider
 export default function WalletConnect() {
-    return (
-        <WagmiConfig client={client}>
-            <Profile />
-        </WagmiConfig>
-    );
+    return <Profile />;
 }
