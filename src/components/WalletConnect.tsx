b39762ff05f94e34e6541ae8431a12620ef03ec1
Ansgar Grunseid <grunseid@gmail.com>
2023-05-12 23:34:04 -0700

    make it clearer what this page is for: claiming saturn node earnings

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -61,7 +61,8 @@ const WalletConnect = () => {
                 rounded="md"
                 spacing="8"
             >
-                <Heading color={'whiteAlpha.900'}>Connect wallet</Heading>
+                <Heading size='2xl' color={'whiteAlpha.900'}>Claim your Saturn node earnings</Heading>
+                <Heading size='lg' color={'whiteAlpha.900'}>To begin, securely connect your Filecoin wallet below</Heading>
 
                 {connectors.map((connector) => (
                     <Button
